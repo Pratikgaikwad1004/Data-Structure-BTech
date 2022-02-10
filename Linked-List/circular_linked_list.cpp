7d605f9d2428c94a60eb72f9627f175faf25ca89
Pratik <Pratikgaikwad1004@gmail.com>
2022-02-10 23:14:34 +0530

    Circular linked list complited

--- Linked-List/circular_linked_list.cpp
+++ Linked-List/circular_linked_list.cpp
@@ -30,10 +30,75 @@ struct Node *insert(int data)
     {
         ptr->next = ptr;
     }
-        head = ptr;
-        return head;
+    head = ptr;
+    return head;
 }
 
+// Insert at first
+struct Node *insertatfirst(int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head;
+    ptr->next = head;
+    do
+    {
+        p = p->next;
+    } while (p->next != head);
+    p->next = ptr;
+    return head;
+}
+
+// Insert at last
+struct Node *insertatlast(int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head;
+    do
+    {
+        p = p->next;
+    } while (p->next != head);
+    p->next = ptr;
+    ptr->next = head;
+    return head;
+}
+
+// Insert at index
+struct Node *insertatindex(int data, int index)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head->next;
+    struct Node *q = head;
+    int i = 0;
+    do
+    {
+        q = q->next;
+        p = p->next;
+    } while (i != index - 1);
+    q->next = ptr;
+    ptr->next = p;
+    return head;
+}
+
+// Insert after node
+struct Node * insertafternode(int data, int node_data){
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head->next;
+    struct Node *q = head;
+    do
+    {
+        q = q->next;
+        p = p->next;
+    } while (q->data != node_data);
+    q->next = ptr;
+    ptr->next = p;
+    return head;
+}
+
+// Display linked list
 void display()
 {
     struct Node *ptr;
@@ -49,7 +114,7 @@ int main()
     int n, x, j, wish, f, index;
     do
     {
-        cout << "1. Create\n2. Display" << endl;
+        cout << "1. Create\n2. Insert at first\n3. Insert at last\n4. Insert at index\n5. Insert after node\n6. Display" << endl;
         cout << "Enter your choice : " << endl;
         cin >> j;
         switch (j)
@@ -65,6 +130,30 @@ int main()
             }
             break;
         case 2:
+            cout << "Enter Node - " << endl;
+            cin >> f;
+            insertatfirst(f);
+            break;
+        case 3:
+            cout << "Enter Node - " << endl;
+            cin >> f;
+            insertatlast(f);
+            break;
+        case 4:
+            cout << "Enter index where you want to insert: " << endl;
+            cin >> index;
+            cout << "Enter Node : " << endl;
+            cin >> f;
+            insertatindex(f, index);
+            break;
+        case 5:
+            cout << "Enter node data where you want to insert: " << endl;
+            cin >> index;
+            cout << "Enter Node : " << endl;
+            cin >> f;
+            insertafternode(f, index);
+            break;
+        case 6:
             cout << "The linked list is: " << endl;
             display();
             break;
